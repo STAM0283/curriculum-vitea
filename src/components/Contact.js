3e9ea432467162a694326161f72882dacfd0daa3
stambouli <aminestambouli90@yahoo.fr>
2021-03-31 17:40:30 +0200

    Change profile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,7 +36,7 @@ const Contact = (props) => {
             object,
             message,
           };
-        axios.post("https://git.heroku.com/sendmail-nodemailer.git/send-email", data)
+        axios.post("https://sendmail-nodemailer.herokuapp.com/send-email", data)
             .then(response => {
                 console.log("my data", response);
                 setSent(response.data);
