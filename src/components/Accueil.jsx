613c34b61255896719776ee00ce5e500f1aad91f
stambouli <aminestambouli90@yahoo.fr>
2021-05-08 16:17:54 +0200

    Change style and translate HOME component

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -10,32 +10,24 @@ const Accueil = (props) => {
     const language = props.data.language;
         const paragraphProfileFr = "Fort de ma passion pour le domaine du développement web et un grand sens de la curiosité, je suis une personne dynamique, rigoureuse, et très investie dans mon travail. je souhaite intégrer une entreprise dynamique, dont le vecteur de réussite et la dimension humaine sont primordiales, mais aussi une entreprise qui saura m'accompagner pour atteindre mon vrai potentiel."
         const paragraphProfileEn = "With my passion for the field of web development and a great sense of curiosity, I am a dynamic, rigorous person, and very invested in my work. I want to integrate a dynamic company, whose vector of success and the human dimension are essential, but also a company which will know how to support me to reach my true potential."
-        const paragraphProfileAr = "مع شغفي بمجال تطوير الويب وشعور كبير بالفضول ، فأنا شخص ديناميكي وصارم ومستثمر للغاية في عملي. أرغب في الانضمام إلى شركة ديناميكية ، والتي يعتبر نجاحها والبعد الإنساني أمرًا أساسيًا ، ولكن أيضًا شركة ستعرف كيف تدعمني للوصول إلى إمكاناتي الحقيقية."
         const nameFr = "STAMBOULI AMINE";
         const nameEn = "STAMBOULI AMINE";
-        const nameAr = "امين اسطامبولي";
         const profileFr = "developpeur React js/Node JS";
         const profilrEn = "React js/Node JS developer";
         const choiceLanguage = () => {
             if (language === "fr") {
                 return paragraphProfileFr
             }
-            else if (language === "en") {
-                return paragraphProfileEn
-            }
             else {
-                return paragraphProfileAr
+                return paragraphProfileEn
             }
         }
         const choiceName = () => {
             if (language === "fr") {
                 return nameFr
             }
-            else if (language === "en") {
-                return nameEn
-            }
             else {
-                return nameAr
+                return nameEn
             }
         }
         const choiceProfile = () => {
