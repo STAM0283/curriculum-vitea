ee8f665625b8117538e86605403ff3294abb4d90
stambouli <aminestambouli90@yahoo.fr>
2021-05-08 23:36:07 +0200

    Add css to project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,22 +6,22 @@ const Experience = (props) => {
   const language = props.data.language;
   return (
     <div className="experience">
-      <div className="experience1" style={{ transition: "all 1s ease-out" }}>
+      <div className="experience1">
         <h2>{language === "fr" ? data.experience3Fr : data.experience3En}</h2>
         <p>{language === "fr" ? data.paragraph3Fr : data.paragraph3En}</p>
 
       </div>
-      <div className="experience2" style={{ transition: "all 1s ease-out" }}>
+      <div className="experience2">
         <h2>{language === "fr" ? data.experience2Fr : data.experience2En}</h2>
         <p>{language === "fr" ? data.paragraph2Fr : data.paragraph2En}</p>
 
       </div>
-      <div className="experience3" style={{ transition: "all 1s ease-out" }}>
+      <div className="experience3">
         <h2>{language === "fr" ? data.experience1Fr : data.experience1En}</h2>
         <p>{language === "fr" ? data.paragraph1Fr : data.paragraph1En}</p>
 
       </div>
-      <div className="experience4" style={{ transition: "all 1s ease-out" }}>
+      <div className="experience4">
         <h2>{language === "fr" ? data.experience4Fr : data.experience4En}</h2>
         <p>{language === "fr" ? data.paragraph4Fr : data.paragraph4En}</p>
       </div>
