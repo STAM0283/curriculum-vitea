0f19ad66d420d96c75e978db5020a0a5021219bf
stambouli <aminestambouli90@yahoo.fr>
2021-05-09 15:03:51 +0200

    some modifications in the training content

--- src/translate/dataTranslate2.js
+++ src/translate/dataTranslate2.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const data = {
     experience1Fr : "Bac +2 - Développeur React JS/ Node JS. A la Wild Code School: de Mai 2020 à deçembre 2020",
-    experience2Fr : "Formations en ligne : Udemy JavaScript, Node Js/Mysql - Depuis Mars 2020.",
+    experience2Fr : "Formations en ligne : Udemy JavaScript, Node Js/Mysql : 2021.",
     experience3Fr : "Maîtrise: Relations et Échanges Internationaux Université Paris 13: 2010-2011",
     experience4Fr : "Master 2 : Socio-Economie des Echanges Université Paris 8 : 2011-2012",
     experience1En : "Bac +2 - React JS / Node JS Developer. At the Wild Code School: from May 2020 to December 2020",
@@ -12,11 +12,14 @@ const data = {
      <strong style={{color: 'red'}}>2)</strong> La maîtrise le développement d'une application : <strong>Node.JS/Express.JS, JavaScript / React JS</strong>.<br /> - 
      <strong style={{color: 'red'}}>3)</strong> Apprendre à concevoir une base de données, écrire et optimiser les requêtes SQL(<strong>Mysql</strong>),<br />
      <strong style={{color: 'red'}}>4)</strong> La maîtriser un gestionnaire de versions (<strong>GIT</strong>)"</p>,
-    paragraph2Fr  : <p>Formation complete sur JavaScript: <br/><br/>
+    paragraph2Fr  : <p>
     <strong style={{color: 'red'}}>1)</strong> la programmation orientée objet avec les fonctions,  constructeurs, les classes.<br/>
-    <strong style={{color: 'red'}}>2)</strong> Les objets littéraux, Les prototypes .etc<br/>
-    <strong style={{color: 'red'}}>3)</strong> La maitrise de Visuel Studio Code, utiliser les API, entre autres</p>,
-    paragraph3Fr  : <p>Ce Master 1 vise à intégrer les étudiants dans des secteurs et des champs caractérisés
+    <strong style={{color: 'red'}}>2)</strong> Les objets littéraux, Les prototypes, les boucles .etc<br/>
+    <strong style={{color: 'red'}}>3)</strong> La maitrise de <strong>Visuel Studio Code</strong>, consommation d'<strong>API</strong>, entre autres.
+    <br/>
+    <strong style={{color: 'red'}}>4)</strong> Création d'<strong>API REST</strong> en <strong>Node JS</strong>, <strong>Express JS</strong>
+     et <strong>Postman</strong></p>,
+    paragraph3Fr  : <p>Cette maîtrise vise à intégrer les étudiants dans des secteurs et des champs caractérisés
     par leur dimension internationale(ONG, diplomatie/affaires étrangères/défense, entreprises multinationales, etc…)</p>,
     paragraph4Fr  : <p>la compréhension et à l’analyse des sociétés du Sud dans leurs mutations, transformations et interrelations,
     en renouvelant les approches et en éprouvant les concepts à partir des disciplines de la sociologie et de la science politique, 
@@ -26,7 +29,13 @@ const data = {
     <strong style={{color: 'red'}}>2)</strong> Learn the development of an application: Node.JS / Express.JS, JavaScript / React. <br />
     <strong style={{color: 'red'}}>3)</strong> Learn to design a database, write and optimize SQL queries (MySQL), <br />
     <strong style={{color: 'red'}}>4)</strong> and use a version manager (GIT)</p>,
-    paragraph2En  : "Complete training on JavaScript: object-oriented programming with functions constructors, with classes, with literal objects, with prototypes .etc <br/>Mastery of Visual Studio Code, use APIs, among others",
+    paragraph2En  : <p>
+    <strong style = {{color: 'red'}}> 1) </strong> object oriented programming with functions, constructors, classes. <br/>
+    <strong style = {{color: 'red'}}> 2) </strong> Literal objects, Prototypes and loops <br/>
+    <strong style = {{color: 'red'}}> 3) </strong> Mastery of <strong> Visual Studio Code </strong>, consumption of <strong> API </strong>, among others.
+    <br/>
+    <strong style = {{color: 'red'}}> 4) </strong> Creation of <strong> API REST </strong> using <strong> Node JS </strong>, <strong> Express JS </ strong>
+     and <strong>Postman</strong> </p>,
     paragraph3En  : "This Master 1 aims to integrate students in sectors and fields characterized through their international dimension (NGOs, diplomacy / foreign affairs / defense, companies  multinationals, etc ...)",
     paragraph4En  : "understanding and analyzing southern societies in their mutations, transformations and interrelationships, by renewing the approaches and testing the concepts from the disciplines of sociology and political science, geography, educational sciences",
  }
